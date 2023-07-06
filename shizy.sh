a44fa81a7efb08443e92d0db96adb2b016c4894f
alvrnz <renz4you26@gmail.com>
2023-07-06 14:09:46 +0700

    Shizy

---
+++ shizy.sh
@@ -0,0 +1,7 @@
+git init
+git config --global --add safe.directory /storage/emulated/0/Shizy
+git add .
+git commit -m "Shizy"
+git branch -M main
+git remote add origin https://github.com/alvrnz/ShizyRobot.git
+git push -u origin main
